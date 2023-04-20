cc69f34b77113d80970649830aa29dd3faf9db2a
ohawaldar <ohawaldar@BMINP5716-2304>
2023-04-20 23:11:07 +0530

    Add project files.

---
+++ useCalculatorLib/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using calculator;
+
+namespace useCalculatorLib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator.mathFuncs cal = new calculator.mathFuncs();
+            Console.WriteLine("Enter the first number : ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second number : ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            
+            cal.Add(A, B);
+            
+            cal.Sub(A, B);
+            
+            cal.mul(A, B);
+            
+            cal.div(A, B);
+
+            Console.ReadLine();
+        }
+    }
+}
